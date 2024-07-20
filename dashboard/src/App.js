bfad05f9bef223c3b8ea0a3c5419d256597968ea
Ivan Doskovic <drndivoje@users.noreply.github.com>
2024-07-20 16:02:15 +0200

    Update dashboard
    - refresh map every 5 seconds

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -9,26 +9,32 @@ import {Marker} from 'react-leaflet/Marker'
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [data, setData] = useState({latitude:52.51632949, longitude:13.37684391});
+  const [data, setData] = useState({latitude:52.51632949, longitude:13.37684391, time: 0});
 
   useEffect(() => {
-    fetch('http://0.0.0.0:8080/dashboard/current')
+    //Implementing the setInterval method
+    const interval = setInterval(() => {
+      fetch('http://0.0.0.0:8080/dashboard/current')
       .then(response => response.json())
       .then(json => setData(json))
       .catch(error => console.error(error));
-  }, []);
+    }, 5000);
+
+    //Clearing the interval
+    return () => clearInterval(interval);
+}, []);
+
   return (
     <div className="App">
-
-      <MapContainer center={[data.latitude, data.longitude]} zoom={13} scrollWheelZoom={false} style={{ height: 536 }}>
+ <MapContainer center={[data.latitude, data.longitude]} zoom={13} scrollWheelZoom={false} style={{ height: 536 }}>
+ <MapRecenter lat={data.latitude} lng={data.longitude} zoomLevel={13} /> 
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
   <Marker position={[data.latitude, data.longitude]}>
     <Popup>
-    latitude :{
-    data.latitude} <br /> longitude :{data.longitude}.
+        Seen at: <br />{new Date(data.time).toLocaleString()}.
     </Popup>
   </Marker>
 </MapContainer>
@@ -37,5 +43,17 @@ function App() {
 
   );
 }
+
+
+const MapRecenter= ({ lat, lng, zoomLevel }) => {
+  const map = useMap();
+
+  useEffect(() => {
+    // Fly to that coordinates and set new zoom level
+    map.flyTo([lat, lng], zoomLevel );
+  }, [lat, lng]);
+  return null;
+
+};
 
 export default App;

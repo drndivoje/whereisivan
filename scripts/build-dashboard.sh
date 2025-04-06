36be246acf23867b79a2f8416cb487dd4fcb2e90
Ivan Doskovic <drndivoje@users.noreply.github.com>
2025-04-06 19:58:07 +0200

    Fix build frontend

--- scripts/build-dashboard.sh
+++ scripts/build-dashboard.sh
@@ -2,11 +2,12 @@
 
 echo "Building Frontend..."
 cd dashboard
-rm -rf build
+rm -rf build 
 npm install > /dev/null 2>&1 && npm run build > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "Build failed"
     exit 1
 fi
+rm -rf ../backend/src/main/resources/dashboard-app
 cp -r build ../backend/src/main/resources/dashboard-app
 echo "Build succeeded"

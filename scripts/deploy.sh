a0865f5012f1d4bfc8f52b000c6fb92a2fef4405
Ivan Doskovic <drndivoje@users.noreply.github.com>
2025-09-08 20:53:22 +0200

    infra
    - fix terraform provider version

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,7 @@
 # Navigate to the infra folder
 cd "$(dirname "$0")/../infra" || exit 1
 
+rm -rf .terraform .terraform.lock.hcl
 terraform init -backend-config="./state.config"
 # Run terraform apply
 terraform apply -auto-approve

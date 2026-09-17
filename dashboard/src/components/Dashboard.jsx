51a784ce3cb35449a70dade30ea8bf88baea65d1
Ivan Doskovic <drndivoje@users.noreply.github.com>
2026-09-17 14:37:13 +0200

    Add REACT_APP_TILE_URL variable for the custom tile server

--- dashboard/src/components/Dashboard.jsx
+++ dashboard/src/components/Dashboard.jsx
@@ -91,7 +91,7 @@ function Dashboard() {
                 <MapRecenter lat={data.latitude} lng={data.longitude} zoomLevel={zoomLevel} />
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-                    url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+                    url={process.env.REACT_APP_TILE_URL || "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"}
                 />
                 <MovingMarker position={[data.latitude, data.longitude]}>
                     <Popup>
